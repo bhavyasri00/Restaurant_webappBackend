7c2f45eb9e6b87cd1ec430a29de1a263a9f61874
bhavyasri00 <alavalabhavyanagasri390@gmail.com>
2025-11-13 19:40:09 +0530

    added

--- models/restaurantModel.js
+++ models/restaurantModel.js
@@ -1,8 +1,8 @@
 import pool from "../config/db.js";
 
-export const createRestaurant = async (userId, name, rating, menu) => {
+export const createRestaurant = async (userId, name, rating) => {
   const result = await pool.query(
-    `INSERT INTO restaurants (user_id, name, rating, menu)
+    `INSERT INTO restaurants (user_id, name, rating)
      VALUES ($1, $2, $3, $4) RETURNING *`,
     [userId, name, rating, JSON.stringify(menu)]
   );
